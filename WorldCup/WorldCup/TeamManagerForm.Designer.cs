b159ce47ea213a1c4c2323027e863488ae780c6b
mentosti <mentosti29071994@gmail.com>
2014-05-18 21:07:48 +0700

    dd

--- WorldCup/WorldCup/TeamManagerForm.Designer.cs
+++ WorldCup/WorldCup/TeamManagerForm.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.lbUsername = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lbUsername
@@ -40,15 +42,25 @@
             this.lbUsername.TabIndex = 1;
             this.lbUsername.Text = "lbUsername";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(70, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
             // TeamManagerForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(514, 347);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lbUsername);
             this.Name = "TeamManagerForm";
             this.Text = "TeamManagerForm";
             this.Load += new System.EventHandler(this.TeamManagerForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,5 +69,6 @@
         #endregion
 
         private System.Windows.Forms.Label lbUsername;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
