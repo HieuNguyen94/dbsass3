a81d7c4b4b42f0c1825cec99caf35047a97bcda5
minhchaupham1994 <phamducminhchau@gmail.com>
2014-05-19 23:00:24 +0700

    chau

--- WorldCup/WorldCup/ReporterForm.Designer.cs
+++ WorldCup/WorldCup/ReporterForm.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.lbUsername = new System.Windows.Forms.Label();
+            this.dataView = new System.Windows.Forms.DataGridView();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataView)).BeginInit();
             this.SuspendLayout();
             // 
             // lbUsername
@@ -40,15 +44,45 @@
             this.lbUsername.TabIndex = 1;
             this.lbUsername.Text = "lbUsername";
             // 
+            // dataView
+            // 
+            this.dataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataView.Location = new System.Drawing.Point(15, 83);
+            this.dataView.Name = "dataView";
+            this.dataView.Size = new System.Drawing.Size(577, 405);
+            this.dataView.TabIndex = 2;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(15, 25);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 39);
+            this.btnInsert.TabIndex = 3;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(119, 25);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 39);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
             // ReporterForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(715, 500);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.dataView);
             this.Controls.Add(this.lbUsername);
             this.Name = "ReporterForm";
             this.Text = "ReporterForm";
             this.Load += new System.EventHandler(this.ReporterForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,5 +91,8 @@
         #endregion
 
         private System.Windows.Forms.Label lbUsername;
+        private System.Windows.Forms.DataGridView dataView;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnDelete;
     }
 }
